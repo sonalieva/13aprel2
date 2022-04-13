c810324fe946898f49783a2924a816c19f93b326
sonalieva <708v8lg@code.edu.az>
2022-04-13 09:49:04 +0400

    created

---
+++ 13aprel/Program.cs
@@ -0,0 +1,42 @@
+﻿using ClassLibrary;
+using System;
+
+namespace _13aprel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Meeting meeting1 = new Meeting();
+            meeting1.Name = "smth1";
+            meeting1.FromDate = new DateTime(2022, 3, 28, 8, 12, 0);
+            meeting1.ToDate = new DateTime(2022, 4, 8, 11, 5, 0);
+
+            Meeting meeting2 = new Meeting();
+            meeting2.Name = "smth2";
+            meeting2.FromDate = new DateTime(2022, 3, 28, 8, 12, 0);
+            meeting2.ToDate = new DateTime(2022, 4, 8, 11, 5, 0);
+
+            MeetingSchedule meetingSchedule = new MeetingSchedule();
+            meetingSchedule.Meetings.Add(meeting1);
+            meetingSchedule.Meetings.Add(meeting2);
+            DateTime from = new DateTime(2022, 3, 28, 11, 28, 0);
+            DateTime to = new DateTime(2022, 4, 13, 11, 10, 0);
+            meetingSchedule.SetMeeting("smth3", from, to);
+
+            
+           
+            foreach (var item in meetingSchedule.GetExistMeetings("smth"))
+            {
+                Console.WriteLine(item.Name);
+            }
+
+
+            Console.WriteLine(meetingSchedule.FindMeetingsCount(new DateTime(2022, 4, 12, 10, 28, 5)));
+           
+            Console.WriteLine(meetingSchedule.IsExistsMeetingByName("smth"));
+
+          
+        }
+    }
+}
---
+++ ClassLibrary/MeetingSchedule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class MeetingSchedule
+    {
+         public List<Meeting> Meetings = new List<Meeting>();
+
+        public void SetMeeting(string fullname, DateTime from, DateTime to)
+        {
+            if (!Meetings.Exists(x => (x.ToDate > x.FromDate) && (x.FromDate < from || x.FromDate < to) ))
+            {
+                Meetings.Add(new Meeting());
+            }
+            else
+            {
+                throw new Exception("Bu vaxt uygun deyil");
+            }
+        }
+        public int FindMeetingsCount(DateTime date)
+        {
+            return Meetings.FindAll(x => x.FromDate > date).Count;
+        }
+        public bool IsExistsMeetingByName(string name)
+        {
+            if( Meetings.Exists(x => x.Name == name)) 
+            {
+                return true;
+            }
+            return false;
+        }
+        public List<Meeting> GetExistMeetings(string name)
+        {
+            return Meetings.FindAll(x => x.Name.Contains(name));
+        }
+    }
+}
